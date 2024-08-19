b681becc74d556a051bf63799719f7a2ede8d87b
ChungyiBossi <chungyi.bossi@gmail.com>
2024-08-19 15:41:25 +0800

    [feature] draw landmarks

--- mediapipe_gesture.py
+++ mediapipe_gesture.py
@@ -24,7 +24,7 @@ options = GestureRecognizerOptions(
 
 with GestureRecognizer.create_from_options(options) as recognizer:
     # Load the input image from an image file.
-    mp_image = mp.Image.create_from_file('images/victory_1.jpg')
+    mp_image = mp.Image.create_from_file('images/victory_2.jpg')
     # 手勢辨識
     gesture_recognition_result = recognizer.recognize(mp_image)
 
@@ -35,3 +35,40 @@ with GestureRecognizer.create_from_options(options) as recognizer:
     for landmark in hand_landmarks:
         print("Landmark: ", round(landmark.x, 3), round(landmark.y, 3), round(landmark.z, 3))
     
+
+# 目的：把拿到的手指節點(hand landmarks)，以及點跟點之間的連結，畫在圖上
+import cv2
+from mediapipe.framework.formats import landmark_pb2 # 型態轉換
+
+# (22-24)
+mp_hands = mp.solutions.hands  # 手部的一些設定
+mp_drawing = mp.solutions.drawing_utils # 繪畫的設定
+mp_drawing_styles = mp.solutions.drawing_styles # 線條的風格
+
+# 型態轉換 (63-66)
+hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+hand_landmarks_proto.landmark.extend([
+    landmark_pb2.NormalizedLandmark(
+        x=landmark.x, y=landmark.y, z=landmark.z
+    ) 
+    for landmark in hand_landmarks
+])
+
+# 把MediaPipe(pillow)圖片 --> cv2 image (numpy array)
+annotate_image = mp_image.numpy_view()[:, :, ::-1].copy()
+
+# (68-73)
+mp_drawing.draw_landmarks(
+    annotate_image,
+    hand_landmarks_proto,
+    mp_hands.HAND_CONNECTIONS,
+    # mp_drawing_styles.get_default_hand_landmarks_style(),
+    # mp_drawing_styles.get_default_hand_connections_style()
+)
+
+# 利用opencv顯示圖片到視窗
+cv2.imshow("Hands", annotate_image)
+key = cv2.waitKey(200000)
+if key == ord('q'):
+    cv2.destroyAllWindows()
+cv2.destroyAllWindows()
