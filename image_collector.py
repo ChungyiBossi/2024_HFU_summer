9a8d244111919b59e14063534e0e84caa9fe8e70
ChungyiBossi <chungyi.bossi@gmail.com>
2024-08-20 11:47:08 +0800

    [feature] display is_collection_start on window

--- image_collector.py
+++ image_collector.py
@@ -9,12 +9,23 @@ while True:
     is_success, frame = camera.read()  # 從camera取得資料
     if is_success:
         # 我想要在這邊可以透過camera蒐集圖片
-        cv2.imshow("Collector", frame)
+        show_frame = frame.copy() # Copy frame for display
+        cv2.putText(
+            img=show_frame,
+            text=f"Collecting:{is_collection_start}",
+            org=(50, 50),  # 圖片的像素坐標系，Y軸是反過來的(向下變大)
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+            fontScale=1,
+            color=(0, 255, 255), 
+            thickness=5, 
+            lineType=cv2.LINE_AA
+        )
+        cv2.imshow("Collector", show_frame)
         if is_collection_start: # 要蒐集
             image_name = f"{time.time()}.jpg" # 我用 timestamp
             filename = f"{folder_path}/{category_name}/{image_name}" # 組合檔名
             cv2.imwrite(filename, frame)
-            key = cv2.waitKey(500)
+            key = cv2.waitKey(100)
         else: # 不蒐集
             key = cv2.waitKey(1)
     else:
