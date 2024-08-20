47a6cacc550f0afbb3e2ea0e3849f8623434ec27
ChungyiBossi <chungyi.bossi@gmail.com>
2024-08-20 14:05:00 +0800

    [refactor]

--- image_collector.py
+++ image_collector.py
@@ -1,6 +1,18 @@
 import cv2
 import time
 
+def put_cv2_text(image, text, org):
+    cv2.putText( 
+        img=image,
+        text=text,
+        org=org,  # 圖片的像素坐標系，Y軸是反過來的(向下變大)
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
+        fontScale=1,
+        color=(0, 255, 255), 
+        thickness=5, 
+        lineType=cv2.LINE_AA
+    )            
+
 def collect_image(folder_path, category_name, camera_id):
     window_name = "Collector"
     camera = cv2.VideoCapture(camera_id)
@@ -10,16 +22,8 @@ def collect_image(folder_path, category_name, camera_id):
         if is_success:
             # 我想要在這邊可以透過camera蒐集圖片
             show_frame = frame.copy() # Copy frame for display
-            cv2.putText(
-                img=show_frame,
-                text=f"Collecting:{is_collection_start}",
-                org=(50, 50),  # 圖片的像素坐標系，Y軸是反過來的(向下變大)
-                fontFace=cv2.FONT_HERSHEY_SIMPLEX, 
-                fontScale=1,
-                color=(0, 255, 255), 
-                thickness=5, 
-                lineType=cv2.LINE_AA
-            )
+            put_cv2_text(show_frame, f"Collecting: {is_collection_start}", (30, 50)) # 顯示出是否蒐集中?
+            put_cv2_text(show_frame, f"Category: {category_name}", (30, 100)) # 顯示出蒐集類別?
             cv2.imshow(window_name, show_frame)
             if is_collection_start: # 要蒐集
                 image_name = f"{time.time()}.jpg" # 我用 timestamp
@@ -38,11 +42,11 @@ def collect_image(folder_path, category_name, camera_id):
             is_collection_start = True
         elif key == ord('z') or key == ord('Z'): # 暫停
             is_collection_start = False
-
     cv2.destroyWindow(window_name)
     
 if __name__ == '__main__':
-    folder_path = 'images/gesture'
-    category_name = 'open_palm'
     camera_id = 0
-    collect_image(folder_path, category_name, camera_id)
+    folder_path = 'images/gesture'
+    categories_name = ['open_palm', 'thumb_up', 'victory']
+    for category_name in categories_name:
+        collect_image(folder_path, category_name, camera_id)
